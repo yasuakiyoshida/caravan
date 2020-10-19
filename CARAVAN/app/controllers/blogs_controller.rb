ce6731f4907e2e1f69fc46f658204c1620dab027
EC2 Default User <ec2-user@ip-172-31-86-149.ec2.internal>
2020-10-19 06:43:29 +0000

    [Finish] 3-7

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -28,7 +28,10 @@ class BlogsController < ApplicationController
   end
   
   def destroy
-    
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blog_path
+  end
   
   private
   def blog_params
